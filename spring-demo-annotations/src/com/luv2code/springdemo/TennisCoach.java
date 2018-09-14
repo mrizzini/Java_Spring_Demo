d12b3035afd8bd0a6916892de7d8e32f14aff447
Matthew Rizzini <matthewrizzini@gmail.com>
2018-09-13 20:32:32 -0400

    completed section 9

--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -2,9 +2,11 @@ package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class TennisCoach implements Coach {
 	
 	@Autowired
@@ -14,6 +16,19 @@ public class TennisCoach implements Coach {
 	public TennisCoach() {
 		System.out.println(">> TennisCoach: inside default constructor");
 	}
+	
+	
+	// define my init method
+//	@PostConstruct
+//	public void doMyStartUpStuff() {
+//		System.out.println(">> TennisCoach: inside of doMyStartUpStuff");
+//	}
+//	
+//	// define my destroy method
+//	@PostDestroy
+//	public void doMyCleanUpStuff() {
+//		System.out.println(">> TennisCoach: inside of doMyCleanUpStuff");
+//	}
 	
 //	// method injection
 //	@Autowired
